eb1da04ed38870b9ac2b7ab550139a7a887d494e
nejdetkadir <nejdetkadir.550@gmail.com>
2021-01-29 17:47:58 +0300

    Create design

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:grade_calculator/model/lesson.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,109 +10,216 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        home: MyHomePage());
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  var formKey = GlobalKey<FormState>();
+  int selectedCourseCredit = 1;
+  double selectedCourseGrade = 4;
+  double average = 0;
+  int uniqID = 1;
+  List<Lesson> createdLessons;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    createdLessons = [
+      Lesson("Matematik", 4, 1, Colors.red),
+      Lesson("Matematik", 3, 1, Colors.orange)
+    ];
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Grade Calculator"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            margin: EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Form(
+                  key: formKey,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.fact_check),
+                      hintText: "Enter your score",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        child: DropdownButtonHideUnderline(
+                          child: DropdownButton<int>(
+                            value: selectedCourseCredit,
+                            items: courseCredits(),
+                            onChanged: (value) {
+                              setState(() {
+                                selectedCourseCredit = value;
+                              });
+                            },
+                          ),
+                        ),
+                      ),
+                      Container(
+                        child: DropdownButtonHideUnderline(
+                          child: DropdownButton<double>(
+                            value: selectedCourseGrade,
+                            items: courseGrades(),
+                            onChanged: (value) {
+                              setState(() {
+                                selectedCourseGrade = value;
+                              });
+                            },
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(18),
+                      ),
+                      color: Colors.blue.shade500),
+                  margin: EdgeInsets.only(top: 15),
+                  height: 50,
+                  child: Center(
+                    child: average != 0
+                        ? Text(
+                            "Your average is : $average",
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          )
+                        : Text(
+                            "Please enter your scores",
+                            style: TextStyle(color: Colors.white, fontSize: 18),
+                          ),
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 15),
+              child: ListView.builder(
+                itemCount: createdLessons.length,
+                itemBuilder: _myListBuilder,
+              ),
             ),
-          ],
-        ),
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: () {
+          debugPrint("onPressed:floatingActionButton");
+        },
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        elevation: 12,
+      ),
     );
+  }
+
+  courseCredits() {
+    List<DropdownMenuItem<int>> data = [];
+    for (int i = 1; i <= 10; i++) {
+      data.add(
+        DropdownMenuItem(
+          value: i,
+          child: Text("$i Credit"),
+        ),
+      );
+    }
+    return data;
+  }
+
+  courseGrades() {
+    List<DropdownMenuItem<double>> data = [];
+    var grades = [
+      ["AA", 4],
+      ["BA", 3.5],
+      ["BB", 3],
+      ["CB", 2.5],
+      ["CC", 2],
+      ["DC", 1.5],
+      ["DD", 1],
+      ["FF", 0]
+    ];
+
+    for (var item in grades) {
+      data.add(
+        DropdownMenuItem(
+          child: Text(item[0]),
+          value: double.parse(item[1].toString()),
+        ),
+      );
+    }
+    return data;
+  }
+
+  Widget _myListBuilder(BuildContext context, int index) {
+    uniqID++;
+    return Dismissible(
+      key: Key(uniqID.toString()),
+      direction: DismissDirection.startToEnd,
+      onDismissed: (direction) {
+        setState(() {
+          createdLessons.removeAt(index);
+        });
+      },
+      child: Card(
+        margin: EdgeInsets.symmetric(vertical: 5),
+        elevation: 8,
+        color: createdLessons[index].color,
+        child: ListTile(
+          title: Text(createdLessons[index].name),
+          subtitle: Text(
+              "Course credit: ${createdLessons[index].courseCredit.toString()}"),
+          leading: CircleAvatar(
+            child: Text(createdLessons[index].name[0]),
+          ),
+          trailing: Text(checkCourseGrade(createdLessons[index].courseGrade)),
+        ),
+      ),
+    );
+  }
+
+  checkCourseGrade(grade) {
+    var data = {
+      4: "AA",
+      3.5: "BA",
+      3: "BB",
+      2.5: "CB",
+      2: "CC",
+      1.5: "DC",
+      1: "DD",
+      0: "FF"
+    };
+    return data[grade];
   }
 }
